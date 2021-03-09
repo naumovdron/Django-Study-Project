23f21d750b5792f779acd1efc14153320285b658
Naumov Andrei <naumov.dron@gmail.com>
2021-03-10 01:03:03 +0300

    updated configs, added wsgi app hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+def hello(environ, start_response):
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
+    start_response(status, headers)
+    return body
